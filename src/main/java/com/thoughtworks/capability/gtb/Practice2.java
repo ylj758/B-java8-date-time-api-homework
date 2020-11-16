58243cad8a642dc46908fab16cfe99a1cb25aea7
ylj758 <758766951@qq.com>
2020-11-16 22:10:49 +0800

    [ylj]:implement Practice1

--- src/main/java/com/thoughtworks/capability/gtb/Practice2.java
+++ src/main/java/com/thoughtworks/capability/gtb/Practice2.java
@@ -9,8 +9,15 @@ import java.time.LocalDate;
  * @create 2020-05-15_17:20
  */
 public class Practice2 {
-
   public static LocalDate getNextWorkDate(LocalDate date) {
-    return null;
+    //1970.1.1周四
+    long leftDay = (date.toEpochDay()-3) % 7;
+    if(leftDay == 5){
+      return date.plusDays(3);
+    }
+    if(leftDay == 6){
+      return date.plusDays(2);
+    }
+    return date.plusDays(1);
   }
 }
